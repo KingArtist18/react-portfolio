82029a62cf6f7c2fa98d6c5614121252bd3d2bd6
Arsal Shaheryar <ArsalShaheryar@outlook.com>
2025-10-03 20:51:06 -0400

    Fix Navbar capitalization issue for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contacts from './pages/Contacts.jsx';
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
